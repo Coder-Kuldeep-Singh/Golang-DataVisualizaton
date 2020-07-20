2725a1fc3b0be7f930e38c8cfef83c72c6a49d95
mavensingh <kuldeep.avsar@gmail.com>
2020-07-20 18:22:18 +0530

    added function to handle no page found

--- controllers/display.go
+++ controllers/display.go
@@ -6,6 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//PageNotFound handle 404
+func PageNotFound(c *gin.Context) {
+	c.JSON(http.StatusNotFound, gin.H{"status": "Failed", "message": "WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND"})
+}
+
 func TableDataSend(c *gin.Context) {
 	// titles := AllTitles()
 	c.HTML(http.StatusOK, "dashboard.html", gin.H{
@@ -13,4 +18,9 @@ func TableDataSend(c *gin.Context) {
 		"top":     TopTen(),
 		"cloud":   CloudChart(),
 	})
+	// c.HTML(http.StatusOK, "script.js", gin.H{
+	// 	"payload": Decoded(),
+	// 	"top":     TopTen(),
+	// 	"cloud":   CloudChart(),
+	// })
 }
