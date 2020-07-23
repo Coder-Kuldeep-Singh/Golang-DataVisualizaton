03328e5b70231ea4cd4d1377b86d600fc7941347
mavensingh <kuldeep.avsar@gmail.com>
2020-07-23 21:04:33 +0530

    added new render function

--- controllers/display.go
+++ controllers/display.go
@@ -11,9 +11,23 @@ func PageNotFound(c *gin.Context) {
 	c.JSON(http.StatusNotFound, gin.H{"status": "Failed", "message": "WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND"})
 }
 
-func TableDataSend(c *gin.Context) {
+func WordCloud(c *gin.Context) {
 	// titles := AllTitles()
-	c.HTML(http.StatusOK, "dashboard.html", gin.H{
+	c.HTML(http.StatusOK, "cloud.html", gin.H{
+		"payload": Decoded(),
+		"top":     TopTen(),
+		"cloud":   CloudChart(),
+	})
+	// c.HTML(http.StatusOK, "script.js", gin.H{
+	// 	"payload": Decoded(),
+	// 	"top":     TopTen(),
+	// 	"cloud":   CloudChart(),
+	// })
+}
+
+func Radar(c *gin.Context) {
+	// titles := AllTitles()
+	c.HTML(http.StatusOK, "radar.html", gin.H{
 		"payload": Decoded(),
 		"top":     TopTen(),
 		"cloud":   CloudChart(),
